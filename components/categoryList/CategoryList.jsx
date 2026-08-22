d324bbaf5c42660a6036d0fa818c35e274fc6085
techking11 <nma1132000@gmail.com>
2026-08-22 11:59:29 +0400

    add: category list

--- components/categoryList/CategoryList.jsx
+++ components/categoryList/CategoryList.jsx
@@ -1,9 +1,31 @@
+import Link from 'next/link'
 import styles from './categorylist.module.css'
+import Image from 'next/image'
 
 function CategoryList() {
   return (
     <div className={styles.container}>
-      Category List Page
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+          <Image src="/style.png" alt="Style" width={32} height={32} className={styles.image} /> Style
+        </Link>
+        <Link href="/blog?cat=fashion" className={`${styles.category} ${styles.fashion}`}>
+          <Image src="/fashion.png" alt="Fashion" width={32} height={32} className={styles.image} /> Fashion
+        </Link>
+        <Link href="/blog?cat=food" className={`${styles.category} ${styles.food}`}>
+          <Image src="/food.png" alt="Food" width={32} height={32} className={styles.image} /> Food
+        </Link>
+        <Link href="/blog?cat=travel" className={`${styles.category} ${styles.travel}`}>
+          <Image src="/travel.png" alt="Travel" width={32} height={32} className={styles.image} /> Travel
+        </Link>
+        <Link href="/blog?cat=culture" className={`${styles.category} ${styles.culture}`}>
+          <Image src="/culture.png" alt="Culture" width={32} height={32} className={styles.image} /> Culture
+        </Link>
+        <Link href="/blog?cat=coding" className={`${styles.category} ${styles.coding}`}>
+          <Image src="/coding.png" alt="Coding" width={32} height={32} className={styles.image} /> Coding
+        </Link>
+      </div>
     </div>
   )
 }
