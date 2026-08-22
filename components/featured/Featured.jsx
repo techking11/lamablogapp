8e3dc0879d202a9a33aa352608f4983b3a5a56c0
techking11 <nma1132000@gmail.com>
2026-08-22 11:27:07 +0400

    add: featured

--- components/featured/Featured.jsx
+++ components/featured/Featured.jsx
@@ -1,9 +1,24 @@
+import Image from 'next/image'
 import styles from './featured.module.css'
 
 function Featured() {
   return (
     <div className={styles.container}>
-      Featured Page
+      <h1 className={styles.title}>
+        <b>Hey, lama dev here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet.</h1>
+          <p className={styles.postDesc}>
+            lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
     </div>
   )
 }
