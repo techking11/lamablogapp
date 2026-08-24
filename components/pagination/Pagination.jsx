e494aaa66c6977e59875d6b6e81088294cc2fed2
techking11 <nma1132000@gmail.com>
2026-08-24 21:32:16 +0400

    add: pagination

--- components/pagination/Pagination.jsx
+++ components/pagination/Pagination.jsx
@@ -3,7 +3,8 @@ import styles from './pagination.module.css'
 function Pagination() {
   return (
     <div className={styles.container}>
-      Pagination Page
+      <button className={styles.button}>Previous</button>
+      <button className={styles.button}>Next</button>
     </div>
   )
 }
