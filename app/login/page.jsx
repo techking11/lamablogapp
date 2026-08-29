b93b85364a90317a0262458ef033471904e9d8c1
techking11 <nma1132000@gmail.com>
2026-08-29 16:06:30 +0400

    add: google auth provider setup

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,20 @@
+import { signIn } from "@/auth"
 import styles from './loginPage.module.css'
+
 function LoginPage() {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton}>Sign in with Google</div>
+        <div className={styles.signInGoogle}>
+          <form
+            action={async () => {
+              "use server"
+              await signIn("google")
+            }}
+          >
+            <button type="submit" className={styles.socialButton}>Signin with Google</button>
+          </form>
+        </div>
         <div className={styles.socialButton}>Sign in with Github</div>
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
